3999372f55f8158691167e552dc49745c9cacd2b
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2022-05-27 10:18:56 +0530

    home page created

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpXToF6k38SH2SQBNy8V4MSxvV8sewwck",
+    authDomain: "clone-71a01.firebaseapp.com",
+    projectId: "clone-71a01",
+    storageBucket: "clone-71a01.appspot.com",
+    messagingSenderId: "845987240640",
+    appId: "1:845987240640:web:4eaea02546b454ea99ab25",
+    measurementId: "G-8X1SX77E5D"
+  };
